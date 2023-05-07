af1d301d79926d0cd7485a2bc6763260f7db1806
kirill2884 <kirill2884@gmail.com>
2023-05-07 15:12:45 +0300

    anumation galery

--- HTML_CSS_JS_INTRODUCTION/src/gramm.js
+++ HTML_CSS_JS_INTRODUCTION/src/gramm.js
@@ -6,7 +6,9 @@ const detailImgElement = document.querySelector(".details-image");
 const detailTitleElement = document.querySelector(".details-title");
 const thumbnailsAnchors = document.querySelectorAll(".thumbnails_ancor");
 const mainSection = document.querySelector("main");
+const detailContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
+const POINT = "point";
 
 for(let i = 0; i < thumbnailsAnchors.length; i++){
     thumbnailsAnchors[i].addEventListener("click",function(){
@@ -15,13 +17,17 @@ for(let i = 0; i < thumbnailsAnchors.length; i++){
 }
 
 function setDetails(anchor){
-    showDetails();
     detailImgElement.src = anchor.getAttribute("data-details-image");
     detailTitleElement.innerHTML = anchor.getAttribute("data-details-text");
+    showDetails();
 }
 
 function showDetails (){
     mainSection.classList.remove(HIDDEN);
+    detailContainer.classList.add(POINT); 
+    setTimeout(function(){
+        detailContainer.classList.remove(POINT); 
+    })
 }
 
 function hideDetails(){
