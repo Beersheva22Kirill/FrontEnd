90840db4ed3d49b1a66b001341402cf6c72709b4
kirill2884 <kirill2884@gmail.com>
2023-05-07 12:35:17 +0300

    full format

--- HTML_CSS_JS_INTRODUCTION/src/gramm.js
+++ HTML_CSS_JS_INTRODUCTION/src/gramm.js
@@ -5,6 +5,8 @@
 const detailImgElement = document.querySelector(".details-image");
 const detailTitleElement = document.querySelector(".details-title");
 const thumbnailsAnchors = document.querySelectorAll(".thumbnails_ancor");
+const mainSection = document.querySelector("main");
+const HIDDEN = "hidden";
 
 for(let i = 0; i < thumbnailsAnchors.length; i++){
     thumbnailsAnchors[i].addEventListener("click",function(){
@@ -13,6 +15,15 @@ for(let i = 0; i < thumbnailsAnchors.length; i++){
 }
 
 function setDetails(anchor){
+    showDetails();
     detailImgElement.src = anchor.getAttribute("data-details-image");
     detailTitleElement.innerHTML = anchor.getAttribute("data-details-text");
+}
+
+function showDetails (){
+    mainSection.classList.remove(HIDDEN);
+}
+
+function hideDetails(){
+    mainSection.classList.add(HIDDEN);
 }
