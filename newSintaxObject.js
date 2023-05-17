4c6071c00d90a977eca70df87ec75b817b6e5b85
kirill2884 <kirill2884@gmail.com>
2023-05-17 12:53:53 +0300

    Old and new sintax object

---
+++ newSintaxObject.js
@@ -0,0 +1,39 @@
+
+
+class Rectangle {
+ 
+    constructor (width,heigth){
+        this.width = width;
+        this.heigth = heigth;   
+    }
+
+    perimetr () {
+        return 2 * (this.heigth + this.width);
+    };
+
+    square (){
+        return this.width * this.heigth;  
+    };
+}
+
+
+console.log('----------------------------------------')
+const rect = new Rectangle(10,20);
+console.log(rect)
+console.log("rectangle (square) = " + rect.square())
+console.log("rectangle (perimetr) = " + rect.perimetr()  + "\n")
+
+
+class Square extends Rectangle{
+ 
+    constructor (width){
+        super(width,width)   
+    }
+}
+
+const square = new Square(5);
+console.log(square);
+console.log("square (square) = " + square.square());
+console.log("square (perimetr) = " + square.perimetr());
+
+
--- this.js
+++ this.js
@@ -12,10 +12,6 @@ const f2 = () => {
 console.log(`f1 call result`, f1());
 console.log(`f2 call result`, f2());
 
-// const f3 = () => {
-//     return this;
-// }
-
 const x = {f3: function(){
     return this; // содержит ссылку на внешний объект (в данном случае на объект x)
 }, f4 : function(){
@@ -28,7 +24,7 @@ console.log(`x.f4 (return x) call result`, x.f4());
 console.log(`x.f5 call result`, x.f5());
 
 console.log('----------rectangle (this)--------------')
-this.width = 15; //this для функционаяльного выражения גв объекте rectangle
+this.width = 15; //this для функционаяльного выражения в объекте rectangle
 this.heigth = 20;
 
 const rectangle = {width: 20, heigth: 20, square: function(){
@@ -74,5 +70,53 @@ function displayPoint(z, t) {
 
 displayPoint(10, 20);
 displayPoint.apply(point, [100, 300]); //вызывает функциональный объект с привязанным объектом в качестве This 
-                                        //!!!доп параметр передается в качестве массива(!!!Не возврачает)
+                                        //!!!доп параметр передается в качестве массива(!!!Не возвращает)
 displayPoint(20, 30);// параметры имеют вес
+
+console.log('------------------------JSON----------------------------')
+
+const rectangle1 = {width: 20, heigth: 20, square: function(){
+    return this.width * this.heigth;
+}, perimetr: () => (this.width + this.heigth) * 2, perimetrFunc: function(){
+    return (this.width + this.heigth) * 2 ;
+}};
+const rectangle2 = {width: 20, heigth: 20, square: function(){
+    return this.width * this.heigth;
+}, perimetr: () => (this.width + this.heigth) * 2, perimetrFunc: function(){
+    return (this.width + this.heigth) * 2 ;
+}};
+
+const rectangle5 = {width: 33, heigth: 20, square: function(){
+    return this.width * this.heigth;
+}, perimetr: () => (this.width + this.heigth) * 2, perimetrFunc: function(){
+    return (this.width + this.heigth) * 2 ;
+}};
+//JSON stringify преобразует состояние объекта (его поля, кроме функциональных полей) в строку JSON "{имя поля: значение,...,имя поля: значение }"
+const rec1 = JSON.stringify(rectangle1);
+const rec2 = JSON.stringify(rectangle2);
+
+console.log(`objects rectangle1 == rectangle2 are equel ${rectangle1 == rectangle2}`)
+console.log(`JOSN objects rectangle1 and rectangle2 are equel ${rec1 == rec2}`)
+
+console.log(JSON.stringify(rectangle1));
+
+const rectangle3 = JSON.parse(rec1); //формирует объект из JSON строки
+const rectangle4 = JSON.parse(JSON.stringify(rectangle5)); //глубокая копия объекта (его состояния) кроме функциональных полей
+console.log(rectangle3);
+console.log(rectangle4);
+
+
+const rectangle7 = {width: 20, 
+    heigth: 20, 
+    square: function(){
+    return this.width * this.heigth;
+}, 
+perimetr: () => (this.width + this.heigth) * 2, 
+perimetrFunc: function(){
+    return (this.width + this.heigth) * 2 ;
+}, fff:{aaa: {ttt:5}, hhhh: 2}
+};
+let rectangle6 = {...rectangle7};
+console.log(rectangle6.fff.aaa.ttt)
+rectangle6 = JSON.parse(JSON.stringify(rectangle7));
+console.log(rectangle6.fff.aaa.ttt)
