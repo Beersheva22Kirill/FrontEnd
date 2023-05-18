518dc31519ba51e601db579ca7c6b923bb911d11
kirill2884 <kirill2884@gmail.com>
2023-05-18 11:35:51 +0300

    movies initialization

---
+++ HTML_CSS_JS_INTRODUCTION/src/films.js
@@ -0,0 +1,6 @@
+{/* <li class="thumbnails_item">
+    <a href="#" class="thumbnails_ancor" data-details-image = "img/galery/1.jpg" data-details-text="The most biuteful image">
+        <img class="thumbnails_image" src="img/galery/1.jpg" alt="">
+        <span class="thumbnails_title">Cat face</span>
+    </a>
+</li> */}
